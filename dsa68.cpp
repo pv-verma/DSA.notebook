b930a2ea47cc535c28a85ad8911369161f80c48b
Prisha Verma <vprisha94@gmail.com>
2026-08-11 00:30:03 +0530

    STL stack

---
+++ dsa68.cpp
@@ -0,0 +1,34 @@
+// STL stacks
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+
+    stack<int> s;
+
+    s.push(1);   // adding element at the top
+    s.push(2);
+    s.push(3);
+
+    cout<< "top: "<<s.top()<<endl; // accessing top element
+    cout<<"Size of stack s: "<<s.size()<<endl;   // checking size of stack
+
+    cout<<"before pop"<<endl;
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    cout<<endl;
+
+    cout<<"after pop"<<endl;
+    while (!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    cout<<endl;
+
+    cout<<"Size of stack s: "<<s.size()<<endl;   // checking size of stack
+
+}
