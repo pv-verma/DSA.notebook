306f70e1f76c04e1a88faca9227f9925b38a14ba
Prisha Verma <vprisha94@gmail.com>
2026-08-26 20:07:14 +0530

    Characters and strings start

---
+++ dsa90.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int getLength(char name[]) {
+    int count = 0;
+    for(int i = 0; name[i] != '\0'; i++){
+        count++;
+    }
+    return count;
+}
+
+int main() {
+
+    char name[12];
+    cout<<"enter name"<<endl;
+    cin>> name;
+    cout<<name<<endl;
+
+    cout<< "Length: " << getLength(name) << endl;
+
+    return 0;
+}
