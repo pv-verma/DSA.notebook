b3260b7de25d41ccddd1999eebd24e40f9c58735
Prisha Verma <vprisha94@gmail.com>
2026-08-28 19:15:40 +0530

    replace spaces

---
+++ dsa92.cpp
@@ -0,0 +1,31 @@
+#include<string>
+#include<iostream>
+using namespace std;
+string replaceSpaces(string &str){
+	string temp = "";
+    
+    for(int i=0; i<str.length(); i++) {
+        if(str[i] == ' '){
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+        }
+        else
+        {
+            temp.push_back(str[i]);
+        }
+    }
+    return temp;
+}
+
+int main() {
+    string s1 = "Mr John Smith";
+    cout << "Original: " << s1 << endl;
+    cout << "Replaced: " << replaceSpaces(s1) << endl;
+
+    string s2 = "  leading and trailing  ";
+    cout << "Original: \"" << s2 << "\"" << endl;
+    cout << "Replaced: \"" << replaceSpaces(s2) << "\"" << endl;
+
+    return 0;
+}
