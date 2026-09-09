bd2a2a86a39a096cf5a15d01f9707af6b9c4f469
Prisha Verma <vprisha94@gmail.com>
2026-09-09 21:21:27 +0530

    wave print 2D array

---
+++ dsa113.cpp
@@ -0,0 +1,51 @@
+// wave printing 2D array
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> wavePrint(vector<vector<int> > arr, int nRows, int mCols)
+{
+    vector<int> ans;
+    
+    for(int col=0; col<mCols; col++) {
+        
+        if( col&1 ) {
+            //odd Index -> Bottom to top
+            
+            for(int row = nRows-1; row>=0; row--) {
+                //cout << arr[row][col] <<" ";
+                ans.push_back(arr[row][col]);
+            }    
+        }
+        else
+        {
+            // 0 or even iondex -> top to bottom
+            for(int row = 0; row<nRows; row++) {
+                //cout << arr[row][col] << " ";
+                ans.push_back(arr[row][col]);
+            }
+        }   
+    }
+    return ans;
+}
+
+int main() {
+    vector<vector<int>> arr = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+        {10, 11, 12}
+    };
+
+    int nRows = arr.size();
+    int mCols = arr[0].size();
+
+    vector<int> result = wavePrint(arr, nRows, mCols);
+
+    cout << "Wave print: ";
+    for (int x : result) cout << x << " ";
+    cout << endl;
+
+    return 0;
+}
