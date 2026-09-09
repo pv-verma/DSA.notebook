a66450beb7618e7df913d8fd01e5d5720d7dfcd1
Prisha Verma <vprisha94@gmail.com>
2026-09-09 21:15:59 +0530

    spiral array

---
+++ dsa112.cpp
@@ -0,0 +1,85 @@
+// spiral array
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+        vector<int> ans;
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int count = 0;
+        int total = row * col;
+
+        // index initialisation
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = row - 1;
+        int endingCol = col - 1;
+
+        while (count < total) {
+
+            // print starting row
+            for (int index = startingCol; count < total && index <= endingCol; index++) {
+                ans.push_back(matrix[startingRow][index]);
+                count++;
+            }
+            startingRow++;
+
+            // print ending column
+            for (int index = startingRow; count < total && index <= endingRow; index++) {
+                ans.push_back(matrix[index][endingCol]);
+                count++;
+            }
+            endingCol--;
+
+            // print ending row
+            for (int index = endingCol; count < total && index >= startingCol; index--) {
+                ans.push_back(matrix[endingRow][index]);
+                count++;
+            }
+            endingRow--;
+
+            // print starting column
+            for (int index = endingRow; count < total && index >= startingRow; index--) {
+                ans.push_back(matrix[index][startingCol]);
+                count++;
+            }
+            startingCol++;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<vector<int>> matrix1 = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    vector<vector<int>> matrix2 = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+
+    vector<int> result1 = sol.spiralOrder(matrix1);
+    vector<int> result2 = sol.spiralOrder(matrix2);
+
+    cout << "Spiral order (3x3): ";
+    for (int x : result1) cout << x << " ";
+    cout << endl;
+
+    cout << "Spiral order (3x4): ";
+    for (int x : result2) cout << x << " ";
+    cout << endl;
+
+    return 0;
+}
